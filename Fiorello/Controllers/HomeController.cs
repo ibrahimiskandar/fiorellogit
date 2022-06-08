72ca9c2ee575412566340eec16fad433540a9f4d
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-08 23:15:39 +0400

    adwda

--- Fiorello/Controllers/HomeController.cs
+++ Fiorello/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiorello.DAL;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,15 @@ namespace Fiorello.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Slide> slides = _context.Slides.ToList();
+            return View(slides);
         }
     }
 }
