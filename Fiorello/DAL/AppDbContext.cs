193dd398841b0dfcd8b916b4889d06c34b3f437a
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-09 00:59:29 +0400

    awwdawd

--- Fiorello/DAL/AppDbContext.cs
+++ Fiorello/DAL/AppDbContext.cs
@@ -12,5 +12,8 @@ namespace Fiorello.DAL
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Slide> Slides { get; set; }
         public DbSet<Summary> Summary { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
     }
 }
