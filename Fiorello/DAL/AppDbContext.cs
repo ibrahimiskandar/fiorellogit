82b0091d797b677fb893888c1d0d799e6aea34f0
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-08 23:27:14 +0400

    allahverdi yerin rahatdi???

--- Fiorello/DAL/AppDbContext.cs
+++ Fiorello/DAL/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace Fiorello.DAL
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Slide> Slides { get; set; }
+        public DbSet<Summary> Summary { get; set; }
     }
 }
