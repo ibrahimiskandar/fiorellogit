a6e47c8fd4017e159e277d371c9da812052daf05
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-09 03:59:48 +0400

    salamatciliqdihelekibirazdadincelmeyegedirem

--- Fiorello/Startup.cs
+++ Fiorello/Startup.cs
@@ -49,7 +49,7 @@ namespace Fiorello
                     );
                 endpoints.MapControllerRoute(
                     name: "areas",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
                     );
             });
         }
