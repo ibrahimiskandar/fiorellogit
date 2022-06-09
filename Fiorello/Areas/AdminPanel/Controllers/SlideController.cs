5d5a621f02a372ef5c4b42bcf40cb0e07f2c5f4b
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-10 03:45:33 +0400

    yenebasladim

---
+++ Fiorello/Areas/AdminPanel/Controllers/SlideController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.Areas.AdminPanel.Controllers
+{
+    public class SlideController : Controller
+    {
+        private AppDbContext _context { get; set; }
+        public SlideController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
