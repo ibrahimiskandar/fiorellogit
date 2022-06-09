97acd21f5e8948c3d8340f776a8e9dae51fb9519
Ibrahim Isgenderov <iisgenderov588@gmail.com>
2022-06-09 18:41:34 +0400

    otaqdadersvarimis

--- Fiorello/Areas/AdminPanel/Controllers/CategoryController.cs
+++ Fiorello/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,17 @@ namespace Fiorello.Areas.AdminPanel.Controllers
     [Area("AdminPanel")]
     public class CategoryController : Controller
     {
+        private AppDbContext _context { get; }
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            return View(_context.Categories.Where(ct=>!ct.IsDeleted));
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
